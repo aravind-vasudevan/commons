48b4ac27b2e6ef99caaf1e8ee8e727932c281293
Aravind Vasudevan <vasudeva@tcd.ie>
2014-05-19 16:10:26 +0100

    Added gnuplot color file

--- Commons.cpp
+++ Commons.cpp
@@ -3,6 +3,55 @@
 
 #include "Commons.hpp"
 
+bool
+fileExists(const char *filename)
+{
+  ifstream ifile(filename);
+  return ifile;
+}
+
+char* 
+substr(const char* str, int start, int end)
+{
+	char* a = new char[1+(end-start)]; 
+	for(int i=start; i<end; i++) 
+		a[i-start] = str[i]; 
+	a[end-start] = '\0'; 
+	return a;
+}
+
+double
+atod(const char* a)
+{
+	double retVal = atoi(a);
+	//cout<<"\ngangnam : "<<" ip= "<<a<<" retVal = "<<retVal;
+	int start = 0;
+	int end = 0;
+	for(int i=0; a[i] != '\0'; i++){ 
+		if(a[i] == '.' && start == 0){
+			start = i+1; 
+			end = i+1;
+		}
+		else if(start != 0)
+			end = i+1;
+			
+/*		else if(start != 0 &&  (a[i] < '0' || a[i] > '9')){ 
+			end = i; 
+			break; 
+		}*/
+	}
+	if(end > start){ 
+		char* decimal = substr(a, start, end); 
+		//cout<<" decimal  = "<<decimal;
+		int dec = atoi(decimal); 
+		int power = end-start; 
+		retVal += ((double)dec)/(pow(10.0, (double)power)); 
+	}
+//	cout<<"\ngangnam : "<<" ip= "<<a<<" retVal = "<<retVal;
+	return retVal;
+}
+
+
 int
 Commons::getdir( string directory, vector<string> &returnFiles )
 {
@@ -13,9 +62,15 @@ Commons::getdir( string directory, vector<string> &returnFiles )
         cout << "Error(" << errno << ") opening " << directory << endl;
         return errno;
     }
+    vector <string> excludeStrings;
+    excludeStrings.push_back(string("."));
+    excludeStrings.push_back(string(".."));
 
     while ((entries = readdir(directoryPointer)) != NULL)
+    {
+    	if(find(excludeStrings.begin(), excludeStrings.end(), string(entries->d_name)) == excludeStrings.end())
         returnFiles.push_back(string(entries->d_name));
+	}
 
     closedir(directoryPointer);
     return 0;
--- Commons.hpp
+++ Commons.hpp
@@ -9,11 +9,83 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <fstream>
+#include <cmath>
+#include <algorithm>
 
-#define _MASTER 0
-#define _SLAVE 1
+// Macro function for debug printing
+#define _ENABLE_DEBUG0
+// #define _ENABLE_DEBUG1
+// #define _ENABLE_DEBUG2
+// #define _ENABLE_DEBUG3
+// #define _ENABLE_DEBUG4
+// #define _ENABLE_DEBUG5
+
+#ifdef _ENABLE_DEBUG0
+	#define DEBUG0(x)	\
+	{					\
+		x; 				\
+		fflush(stdout); \
+	}
+#else
+	#define DEBUG0(x)
+#endif
+
+#ifdef _ENABLE_DEBUG1
+	#define DEBUG1(x)	\
+	{					\
+		x; 				\
+		fflush(stdout); \
+	}
+#else
+	#define DEBUG1(x)
+#endif
+
+#ifdef _ENABLE_DEBUG2
+	#define DEBUG2(x)	\
+	{					\
+		x; 				\
+		fflush(stdout); \
+	}
+#else
+	#define DEBUG2(x)
+#endif
+
+#ifdef _ENABLE_DEBUG3
+	#define DEBUG3(x)	\
+	{					\
+		x; 				\
+		fflush(stdout); \
+	}
+#else
+	#define DEBUG3(x)
+#endif
+
+#ifdef _ENABLE_DEBUG4
+	#define DEBUG4(x)	\
+	{					\
+		x; 				\
+		fflush(stdout); \
+	}
+#else
+	#define DEBUG4(x)
+#endif
+
+#ifdef _ENABLE_DEBUG5
+#define DEBUG5(x)	\
+{					\
+	x; 				\
+	fflush(stdout); \
+}
+#else
+	#define DEBUG5(x)
+#endif
 
 using namespace std;
+
+char* substr(const char* str, int start, int end);
+bool fileExists(const char *filename);
+double atod(const char* a);
 
 class Commons
 {
